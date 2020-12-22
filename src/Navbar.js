5ccaf3b5deea1f4ac96b64c0f3aa90c36248a321
FlagElitov <61709509+FlagElitov@users.noreply.github.com>
2020-12-22 15:32:50 +0200

    add navbar

---
+++ src/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+const Navbar = ({ title }) => {
+  return (
+    <View style={styles.navbar}>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  navbar: {
+    height: 80,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    backgroundColor: "#fff",
+    paddingBottom: 10,
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 28,
+    fontWeight: "700",
+    color: "#000",
+  },
+});
+
+export default Navbar;
