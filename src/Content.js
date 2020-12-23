ba4ffc4385b6695959e08499a24a4be9e0863884
FlagElitov <61709509+FlagElitov@users.noreply.github.com>
2020-12-23 09:49:57 +0200

    Revert "fix scrollview"
    This reverts commit b2b0030f6ffdbd8be9c382bd2fb59849149e70c9.

--- src/Content.js
+++ src/Content.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 const Content = ({ state }) => {
   return (
     <View style={styles.content}>
-      <ScrollView contentContainerStyle={styles.contentContainer}>
+      <ScrollView>
         <Image
           style={styles.img}
           source={{
@@ -14,41 +14,47 @@ const Content = ({ state }) => {
         />
 
         <Text style={styles.title}>
-          Name : {state ? state.title : "Нет данных , подождите"}
+          Name : {state ? state.title : "Trifid Pillars and Jets"}
         </Text>
 
-        <Text style={styles.text}>Date : {state && state.date}</Text>
         <Text style={styles.text}>
-          Service_version : {state && state.service_version}
+          Date : {state ? state.date : "2020-12-22"}
         </Text>
-        <Text style={styles.text}>{state && state.explanation}</Text>
+        <Text style={styles.text}>
+          Service_version : {state ? state.service_version : "v1"}
+        </Text>
+        <Text style={styles.text}>
+          {state
+            ? state.explanation
+            : "Dust pillars are like interstellar mountains. They survive because they are more dense than their surroundings, but they are being slowly eroded away by a hostile environment. Visible in the featured picture is the end of a huge gas and dust pillar in the Trifid Nebula (M20), punctuated by a smaller pillar pointing up and an unusual jet pointing to the left. Many of the dots are newly formed low-mass stars. A star near the small pillar's end is slowly being stripped of its accreting gas by radiation from a tremendously brighter star situated off the top of the image. The jet extends nearly a light-year and would not be visible without external illumination. As gas and dust evaporate from the pillars, the hidden stellar source of this jet will likely be uncovered, possibly over the next 20,000 years. Growing Gallery: Notable images of the Great Conjunction submitted to APOD"}
+        </Text>
       </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  contentContainer: {
-    paddingVertical: 0,
-  },
   content: {
     padding: 10,
     alignItems: "center",
-    backgroundColor: "#FDFDFD",
+    backgroundColor: "#fff",
     paddingBottom: 10,
     flexGrow: 1,
     paddingBottom: 190,
   },
   title: {
     fontSize: 19,
-    width: 330,
   },
   text: {
     fontSize: 17,
     marginTop: 7,
     width: 330,
   },
-
+  scroll: {
+    flexGrow: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
   img: {
     height: 300,
     width: 400,
